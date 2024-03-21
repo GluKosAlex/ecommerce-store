614c05639a7e961edab43418943fbbc9a7334ed4
GluKosAlex <glukosalex@gmail.com>
2024-03-22 01:45:31 +0700

    feat: add product types

--- types/types.d.ts
+++ types/types.d.ts
@@ -9,3 +9,31 @@ interface ICategory {
   name: string;
   billboard: Billboard;
 }
+
+interface IProduct {
+  id: string;
+  category: ICategory;
+  name: string;
+  price: string;
+  isFeatured: boolean;
+  size: ISize;
+  color: IColor;
+  images: IImage[];
+}
+
+interface IImage {
+  id: string;
+  url: string;
+}
+
+interface IColor {
+  id: string;
+  name: string;
+  value: string;
+}
+
+interface ISize {
+  id: string;
+  name: string;
+  value: string;
+}
