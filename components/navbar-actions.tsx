79ac644a36010d396c8e95465e351c8b3516b0fc
GluKosAlex <glukosalex@gmail.com>
2024-03-23 10:48:47 +0700

    feat: add cart page

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { ShoppingBag } from 'lucide-react';
 
 import Button from '@/components/ui/button';
-import { ShoppingBag } from 'lucide-react';
 import useCart from '@/hooks/use-cart';
 
 const NavbarActions = () => {
@@ -13,13 +14,14 @@ const NavbarActions = () => {
     setMounted(true);
   }, [setMounted]);
 
+  const router = useRouter();
   const cart = useCart();
 
   if (!isMounted) return null;
 
   return (
     <div className='flex items-center gap-x-4 ml-auto'>
-      <Button className='flex items-center px-4 py-2'>
+      <Button onClick={() => router.push('/cart')} className='flex items-center px-4 py-2'>
         <ShoppingBag size={20} color='white' />
         <span className='ml-2 text-white text-sm font-medium'> {cart.items.length}</span>
       </Button>
